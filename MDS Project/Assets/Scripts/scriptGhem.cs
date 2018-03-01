6b67ed8eaf36a1265e5b178daf47abe8f33b1445
EduardValentin <eduard.valentin96@yahoo.it>
2018-03-01 18:19:39 +0200

    Nimic important

---
+++ MDS Project/Assets/Scripts/scriptGhem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptGhem : MonoBehaviour {
+
+    // Use this for initialization
+    Rigidbody2D mRbody;             // Pt iulian, m-ul din fata e o conventie ca datele membru sa fie cu m , te pup :* 
+
+    void Start()
+    {
+        mRbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && mRbody.velocity == new Vector2(0, 0))
+        {
+            mRbody.AddForce(new Vector2(300, 800));
+        }
+
+    }
+}
