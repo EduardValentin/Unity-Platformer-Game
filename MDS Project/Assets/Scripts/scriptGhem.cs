b9546883b7145067749a9e4dbe6946e752bebcec
Eduard Valentin <eduard.valentin96@yahoo.it>
2018-03-04 13:46:03 +0200

    .

--- MDS Project/Assets/Scripts/scriptGhem.cs
+++ MDS Project/Assets/Scripts/scriptGhem.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class scriptGhem : MonoBehaviour {
 
     // Use this for initialization
-    Rigidbody2D mRbody;             // Pt iulian, m-ul din fata e o conventie ca datele membru sa fie cu m , te pup :* 
+    Rigidbody2D mRbody;             // Pt iulian, m-ul din fata e o conventie ca datele membru sa fie cu m , te pup :*  . 
 
     void Start()
     {
@@ -15,7 +15,7 @@ public class scriptGhem : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") && mRbody.velocity == new Vector2(0, 0))
+        if (Input.GetKeyDown("space"))
         {
             mRbody.AddForce(new Vector2(300, 800));
         }
