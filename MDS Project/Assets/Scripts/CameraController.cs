ba30609466231e72abbbad2fbc95e7098a51642c
PetruGit <petru.avramescu.study@gmail.com>
2018-03-26 15:06:23 +0300

    dsa

--- MDS Project/Assets/Scripts/CameraController.cs
+++ MDS Project/Assets/Scripts/CameraController.cs
@@ -13,7 +13,7 @@ public class CameraController : MonoBehaviour {
     private void Start () {
 
         mCharacter = GameObject.FindGameObjectWithTag("Player");
-        mCharacterController.GameObject.GetComponent<CharacterController>();
+		mCharacterController = mCharacter.GetComponent<CharacterController>();
 
     }
 
